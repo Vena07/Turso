287075a3aadfd4b241e487fb0ea9c7973a582af0
Vena07 <vasiknosek@email.cz>
2024-11-19 23:13:56 +0100

    Update schema.ts

--- my-app/src/lib/server/db/schema.ts
+++ my-app/src/lib/server/db/schema.ts
@@ -11,16 +11,4 @@ export const user = mysqlTable('user', {
   age: int('age')
 });
 
-// Definice tabulky `product`
-export const product = mysqlTable('product', {
-
-	// Sloupec `id`, automaticky inkrementovaný primární klíč
-	id: serial('id').primaryKey(),
-  
-	// Sloupec `name`, text představující název produktu
-	name: varchar('name', 255),
-  
-	// Sloupec `price`, desetinné číslo představující cenu produktu
-	price: decimal('price', 10, 2)
-  });
   
